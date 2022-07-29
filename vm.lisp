9a5ec203d5ad97343c25fde9dd7aba7596de6d6c
Bike <aeshtaer@gmail.com>
2022-07-29 09:04:44 -0400

    Nonlocal exit instructions

--- vm.lisp
+++ vm.lisp
@@ -17,6 +17,9 @@
     +make-closure+
     +return+
     +jump-if+
+    +block-open+ +return-from+ +block-close+
+    +tagbody-open+ +go+ +tagbody-close+
+    +special-bind+ +symbol-value+ +symbol-value-set+ +unbind+
     +nil+
     +eq+))
 
@@ -38,7 +41,19 @@
 
 (defvar *trace* nil)
 
-(defun vm (bytecode stack closure constants &key (ip 0) (sp 0) (mv nil) &aux (bp sp))
+(defstruct dynenv)
+(defstruct (block-dynenv (:include dynenv)
+                         (:constructor make-block-dynenv (fun)))
+  (fun (error "missing arg") :type function))
+(defstruct (tagbody-dynenv (:include dynenv)
+                           (:constructor make-tagbody-dynenv (fun)))
+  (fun (error "missing arg") :type function))
+(defstruct (sbind-dynenv (:include dynenv)
+                         (:constructor make-sbind-dynenv ())))
+
+(defvar *dynenv* nil)
+
+(defun vm (bytecode stack closure constants &key (ip 0) (sp 0) (bp sp) &aux (mv nil))
   (declare (type (simple-array (unsigned-byte 8) (*)) bytecode)
            (type (simple-array t (*)) stack closure constants)
            (type (and fixnum (integer 0)) start sp bp)
@@ -58,6 +73,9 @@
            (next-code ()
              ;;(declare (optimize safety 0))
              (aref bytecode (incf ip)))
+           (acode (ip)
+             ;;(declare (optimize (safety 0)))
+             (aref bytecode ip))
            (constant (index)
              ;;(declare (optimize (safety 0)))
              (svref constants index))
@@ -65,14 +83,18 @@
              ;;(declare (optimize (safety 0)))
              (svref closure index))
            (gather (n)
-             (loop repeat n collecting (spop))))
+             (let ((result nil)) ; put the most recent value on the end
+               (loop repeat n do (push (spop) result))
+               result)))
     #+(or)
     (declare (inline stack (setf stack) spush spop
-                     code next-code constant closure))
+                     code next-code acode constant closure))
     (loop with end = (length bytecode)
           until (eql ip end)
           when *trace*
-            do (print (list (decode (code)) stack sp))
+            do (print (list (decode (code))
+                            (subseq stack 0 bp)
+                            (subseq stack bp sp)))
           do (ecase (code)
                ((#.+ref+) (spush (stack (- bp (next-code) 1))) (incf ip))
                ((#.+const+) (spush (constant (next-code))) (incf ip))
@@ -94,7 +116,7 @@
                ((#.+set+)
                 (setf (stack (- bp (next-code) 1)) (spop))
                 (incf ip))
-               ((#.+make-cell+) (spush (make-cell nil)) (incf ip))
+               ((#.+make-cell+) (spush (make-cell (spop))) (incf ip))
                ((#.+cell-ref+) (spush (cell-value (spop))) (incf ip))
                ((#.+cell-set+)
                 (let ((val (spop))) (setf (cell-value (spop)) val))
@@ -111,5 +133,45 @@
                 (return (values-list (if (eql bp sp) mv (gather (- sp bp))))))
                ((#.+jump-if+)
                 (setf ip (if (spop) (next-code) (+ 2 ip))))
+               ((#.+block-open+)
+                (setf mv
+                      (block nil
+                        (let ((*dynenv*
+                                (make-block-dynenv
+                                 (lambda (mv) (return (values-list mv))))))
+                          (spush *dynenv*)
+                          (multiple-value-list
+                           (vm bytecode stack closure constants
+                               ;; +2 to skip over this instruction, including the label
+                               :ip (+ 2 ip) :sp sp :bp bp))))
+                      ip (next-code)))
+               ((#.+return-from+) (funcall (block-dynenv-fun (spop)) mv))
+               ((#.+block-close+) (return (values-list mv)))
+               ((#.+tagbody-open+)
+                (let ((next-ip (+ ip (next-code) 2)) ; the tagbody's prefix
+                      (*dynenv* *dynenv*))
+                  (tagbody
+                     (setf *dynenv*
+                           (make-tagbody-dynenv
+                            (lambda (n new-sp)
+                              (setf next-ip (acode (+ ip n 1)) sp new-sp)
+                              (go loop))))
+                     (spush *dynenv*)
+                   loop
+                     (setf (values ip sp)
+                           (vm bytecode stack closure constants
+                               :ip next-ip :sp sp :bp bp)))))
+               ((#.+go+) (funcall (tagbody-dynenv-fun (spop)) (next-code) sp))
+               ((#.+tagbody-close+) (return (values (1+ ip) sp)))
+               ((#.+special-bind+)
+                (let ((*dynenv* (make-sbind-dynenv)))
+                  (progv (list (constant (next-code))) (list (spop))
+                    (setf ip
+                          (vm bytecode stack closure constants :ip ip :sp sp :bp bp)))))
+               ((#.+symbol-value+) (symbol-value (constant (next-code))) (incf ip))
+               ((#.+symbol-value-set+)
+                (setf (symbol-value (constant (next-code))) (spop))
+                (incf ip))
+               ((#.+unbind+) (return (1+ ip)))
                ((#.+nil+) (spush nil) (incf ip))
                ((#.+eq+) (spush (eq (spop) (spop))) (incf ip))))))
