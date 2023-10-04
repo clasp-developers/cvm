3241ac75579009d49c450a151f5dc9586b2b102d
Alex Wood <aeshtaer@gmail.com>
2023-10-04 08:29:13 -0400

    More documentation for CVM/COMPILE

--- compile/documentation.lisp
+++ compile/documentation.lisp
@@ -10,9 +10,15 @@
       "Compile LAMBDA-EXPRESSION into a function in the compilation ENVIRONMENT.
 Returns three values: FUNCTION, WARNINGSP, FAILUREP, which are the same as those of CL:COMPILE. FUNCTION may or may not be considered a COMPILED-FUNCTION by the host - this is out of CVM's control.
 Unlike CL:COMPILE, COMPILE does not accept a function name, and does not alter bindings to install the newly compiled function."
+      (documentation 'macroexpand-1 'function)
+      "As CL:MACROEXPAND-1. Uses Trucler to interrogate for macro definitions, so it is (probably) more flexible than the host function."
+      (documentation 'macroexpand-1 'function)
+      "As CL:MACROEXPAND. Uses Trucler to interrogate for macro definitions, so it is (probably) more flexible than the host function."
       (documentation 'compile-into 'function)
       "Compile LAMBDA-EXPRESSION in the compilation ENVIRONMENT into a new compiler function within MODULE. The compiler function information is returned, but is not loaded into being an actual callable function. COMPILE-INTO underlies all other compilation operations and can be used for lower level purposes, such as getting a compiler function to serialize during COMPILE-FILE.
-If FORMS-ONLY is provided, the body of the lambda expression is treated as if it was in a PROGN, so declaration expressions are not permitted (and will be treated as forms). If BLOCK-NAME is provided, a block of that name will be established around the body of the lambda expression. These options are available so that functions can be compiled with implicit blocks or progns regardless of the compilation environment's bindings of CL:PROGN or CL:BLOCK."
+If FORMS-ONLY is provided, the body of the lambda expression is treated as if it was in a PROGN, so declaration expressions are not permitted (and will be treated as forms). If BLOCK-NAME is provided, a block of that name will be established around the body of the lambda expression. These options are available so that functions can be compiled with implicit blocks or progns regardless of the compilation environment's bindings of CL:PROGN or CL:BLOCK.
+
+See CMODULE"
       (documentation 'link 'function)
       "Finish compilation of a module by resolving all temporary labels into actual positions. Return the final bytecode for the module."
       (documentation 'with-compilation-results 'function)
@@ -28,4 +34,41 @@ For now in CVM, these actions are only signaling certain kinds of warnings relat
       (documentation 'run-time-environment 'function)
       "The compiler calls this function to get the run time environment for a given compilation environment. Clients should customize it in accordance with their environments' structures."
       (documentation 'resolve-reference 'function)
-      "This function can be called by WITH-COMPILATION-UNIT's expansion when it receives an UNKNOWN-REFERENCE-RESOLUTION signal. The function is called with the resolution condition and any unresolved condition. If the resolution resolves the given condition, T should be returned. Other effects are permissible, such as signaling warnings.")
+      "This function can be called by WITH-COMPILATION-UNIT's expansion when it receives an UNKNOWN-REFERENCE-RESOLUTION signal. The function is called with the resolution condition and any unresolved condition. If the resolution resolves the given condition, T should be returned. Other effects are permissible, such as signaling warnings."
+      (documentation 'cfunction 'type)
+      "A compiler function, i.e. compiler information about an eventual function. After being produced by COMPILE-INTO, contains enough information to make an actual function out of. Compiler functions are used by the file compiler.
+
+See COMPILE-INTO"
+      (documentation 'cfunction-cmodule 'function)
+      "Return a compiler function's module.
+
+See CFUNCTION
+See CMODULE"
+      (documentation 'cfunction-final-entry-point 'function)
+      "Return the computed entry point for a compiler function. This is an index into the bytecode indicating the first instruction to be executed when the eventual function is called.
+
+See CFUNCTION
+See LINK"
+      (documentation 'cfunction-final-size 'function)
+      "Return the size in bytes of the bytecode corresponding to a compiler function.
+
+See CFUNCTION"
+      (documentation 'cmodule 'type)
+      "A compiler module. Represents a collection of functions being compiled together; for example local functions defined by FLET or LAMBDA will share a module with their parent function.
+
+See MAKE-CMODULE
+See CMODULE-LITERALS
+See CFUNCTION
+See COMPILE-INTO"
+      (documentation 'make-cmodule 'function)
+      "Create a new, empty compiler module. Functions can be compiled into the module with COMPILE-INTO.
+
+See CMODULE"
+      (documentation 'cmodule-literals 'function)
+      "Retrieve the literals vector of a CMODULE. This is a vector containing information about constants, LOAD-TIME-VALUE forms, etc. present in compiled code.
+
+See CMODULE"
+      (documentation 'link 'function)
+      "Finish compilation of the MODULE, computing the final, linked bytecode. Does not load constants.
+
+See CMODULE")
